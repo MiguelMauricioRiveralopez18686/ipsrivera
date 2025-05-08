989aad298492ff751ac2807c706228e11e8a9162
Miguel Mauricio Rivera López <91698396+MiguelMauricioRiveralopez18686@users.noreply.github.com>
2025-05-07 20:15:56 -0500

    Delete js directory

--- js/navegar-content.js
+++
@@ -1,9 +0,0 @@
-const navLinks = document.querySelectorAll('#nav-menu #nav-menu li a');
-
-navLinks.forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const section = document.querySelector(link.getAttribute('#'));
-    section.scrollIntoView({behavior: 'smooth'});
-  });
-});
